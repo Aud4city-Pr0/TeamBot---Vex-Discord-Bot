816119b4aa1e0817a8df721280c9a5c1a7712dfc
Aud4city-Pr0 <zachary.duriancik@gmail.com>
2025-11-22 17:15:13 -0500

    Inital Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+### WELCOME TO TeamBot! A DISCORD BOT FOR VEX ROBOTEVENTS! ###
+This is the main repo for this bot!
---
+++ main.py
@@ -0,0 +1,66 @@
+# The main file of the project
+
+# imports, the requests part of the bot will be handled in another script
+# discord api
+import discord
+from discord.ext import commands
+
+# logging and enviroment variable loading
+import logging
+from dotenv import load_dotenv
+import os
+
+# random join messages
+import random
+
+BOT_GREATING = [
+    "Hi there, welcome to the sever!",
+    "Welcome to the server :D, have fun!",
+    "Hello and welcome!",
+    "Hey! Glad you could join us!"
+]
+
+# loading enviorment vars
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
+
+# setting up logging
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+# bot setup
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+# the bot events
+@bot.event
+async def on_ready():
+    print(f"Bot: {bot.user} is ready to use.")
+
+@bot.event
+async def on_member_join(member):
+    await member.send(BOT_GREATING[random.randint(0, len(BOT_GREATING))])
+
+# command handeling
+@bot.command()
+async def info(ctx):
+    await ctx.send(f"""{ctx.author.mention}, you have requseted help about how to use me, here are my commands:
+                   \n !info - displays commands that are used with this bot
+                   \n !team - looks for vex teams based of their number and shows statistics about them (eg. 1234D)
+                   \n !event - looks for events based of their name or events in a season if one is provided
+                   \n !version - says the current version of the bot
+                   \n !skills - gets skills information about a team from a certain season
+                   \n !awards - gets award information about a team from a certian season""")
+
+@bot.command()
+async def version(ctx):
+    #TODO: put verison info in a .json file before realsing bot
+    await ctx.send("TeamBot - A VEX Discord bot, created by: Zach D, version: 0.1.0")
+
+@bot.command()
+async def team(ctx, team_name):
+     await ctx.send(f"Placeholder text: {team_name}")
+
+# running the bot
+bot.run(token, log_handler=handler, log_level=logging.DEBUG)
